e6c4143010367851dc39468a3a982a8321b7ff0d
ttentau <ttentau@163.com>
2018-11-02 23:53:34 +0800

    添加项目变更

---
+++ src/views/project/dialog/ProjectChange.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="ProjectChange">
+        
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ProjectChange",
+        data() {
+            return {}
+        },
+        created() {
+
+        },
+        methods: {},
+        filter: {},
+        computed: {}
+    }
+</script>
+
+<style scoped land='scss'>
+
+</style>
