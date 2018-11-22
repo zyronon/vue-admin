22e8d0ac7b49274b266d4c47a1195bbd07ca2123
ttentau <ttentau@163.com>
2018-11-23 05:15:55 +0800

    修改package

--- src/views/approval/Create.vue
+++ src/views/approval/Create.vue
@@ -5,7 +5,7 @@
             </el-button>
         </el-row>
         <el-card class="box-card">
-            <el-form :inline="true" :model="formInline" class="demo-form-inline">
+            <el-form :inline="true" :model="formInline" class="demo-form-inline mb20p">
                 <el-form-item label="审批流程名称">
                     <el-input v-model="formInline.user" placeholder="审批人"></el-input>
                 </el-form-item>
@@ -16,35 +16,31 @@
                     </el-select>
                 </el-form-item>
             </el-form>
-            <div v-for="(item , index) in list">
-                <!--<el-row class="mb10p mt10p">-->
-                <!--<el-button type="primary" icon="el-icon-circle-plus-outline" @click="addLevel()">-->
-                <!--</el-button>-->
-                <!--<el-button type="danger" icon="el-icon-delete" @click="delLevel(index)" v-if="index!=0"></el-button>-->
-                <!--</el-row>-->
-                <div class="item oh" v-on:mouseleave="hover($event,1,index)" v-on:mouseenter="hover($event,0,index)">
+            <div class="processes">
+                <div v-for="(item , index) in list" class="item oh" v-on:mouseleave="hover($event,1,index)"
+                     v-on:mouseenter="hover($event,0,index)">
                     <i class="el-icon-delete delete" @click="delLevel(index)"/>
                     <div class="w5 pull-left left" ref="left">
                         {{index+1}}级
                     </div>
                     <div class="w93 pull-left right" ref="right">
                         <el-form :inline="true" :model="formInline" class="demo-form-inline">
-                            <el-form-item label="审批类型">
+                            <el-form-item label="审批类型：">
                                 <el-select v-model="formInline.region" placeholder="活动区域">
                                     <el-option label="普通" value="shanghai"></el-option>
                                     <el-option label="会签" value="beijing"></el-option>
                                 </el-select>
                             </el-form-item>
-                            <el-form-item label="审批人员类型">
+                            <el-form-item label="审批人员类型：">
                                 <el-select v-model="formInline.region" placeholder="活动区域">
-                                    <el-option label="区域一" value="shanghai"></el-option>
-                                    <el-option label="区域二" value="beijing"></el-option>
+                                    <el-option label="指定人员" value="shanghai"></el-option>
+                                    <el-option label="指定职务" value="beijing"></el-option>
                                 </el-select>
                             </el-form-item>
-                            <el-form-item label="审批人员">
+                            <el-form-item label="审批人员：">
                                 <el-input v-model="formInline.user" placeholder="审批人"></el-input>
                             </el-form-item>
-                            <el-form-item label="审批通过">
+                            <el-form-item label="审批通过：">
                                 <el-select v-model="formInline.region" placeholder="活动区域">
                                     <el-option label="区域一" value="shanghai"></el-option>
                                     <el-option label="区域二" value="beijing"></el-option>
@@ -54,6 +50,7 @@
                         </el-form>
                     </div>
                 </div>
+                <div class="level-line"></div>
             </div>
             <div class="add" @click="addLevel()">
                 <i class="el-icon-plus"/>添加审批级别
@@ -124,37 +121,51 @@
 </script>
 
 <style scoped lang='scss'>
-    .item {
-        cursor: pointer;
+    .processes {
         position: relative;
-        margin: 20px 0;
-        border: 1px solid gainsboro;
-        border-radius: 3px;
-        box-shadow: 0 1px 6px rgba(0, 0, 0, 0.2) !important;
+        .item {
+            z-index: 1;
+            margin-bottom: 40px;
+            cursor: pointer;
+            position: relative;
+            border: 1px solid gainsboro;
+            border-radius: 3px;
+            box-shadow: 0 1px 6px rgba(0, 0, 0, 0.2) !important;
 
-        &:hover {
-            border: 1px solid #24a4ff;
+            &:hover {
+                border: 1px solid #24a4ff;
+            }
+
+            .left {
+                background: #f5f5f5;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                font-size: 22px;
+            }
+
+            .right {
+                padding: 20px 0 20px 2%;
+            }
+
+            .delete {
+                display: none;
+                position: absolute;
+                right: 10px;
+                top: 10px;
+                font-size: 22px;
+                color: red;
+            }
         }
 
-        .left {
-            background: #f5f5f5;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            font-size: 22px;
-        }
-
-        .right {
-            padding: 20px 0 20px 2%;
-        }
-
-        .delete {
-            display: none;
+        .level-line {
+            height: 100%;
+            border-left: 1px dashed #979797;
             position: absolute;
-            right: 10px;
-            top: 10px;
-            font-size: 22px;
-            color: red;
+            top: 0;
+            bottom: 100px;
+            left: 40px;
+            width: 0;
         }
     }
 
@@ -174,4 +185,5 @@
             color: #0088ff;
         }
     }
+
 </style>
