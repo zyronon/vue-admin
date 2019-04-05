bc10a67e046152d670a720676cb532b3c5d1ecbe
ttentau <ttentau@163.com>
2019-04-05 22:52:17 +0800

    reset

--- src/utils/import.js
+++
@@ -1,7 +0,0 @@
-export default function _import(file, viewPath = 'views') {
-    if (process.env.NODE_ENV === 'development') {
-        return require(`@/${viewPath}/${file}.vue`).default
-    } else if (process.env.NODE_ENV === 'production') {
-        return () => import(`@/${viewPath}/${file}.vue`)
-    }
-}
