7d7dd49019d97cce2ce59bd01c0d019900b7eefd
ttentau@163.com <ttentau@163.com>
2020-02-14 05:01:49 +0800

    bf

--- src/utils/import.js
+++ src/utils/import.js
@@ -1,11 +1,14 @@
 // 如果是开发环境普通导入
 // 如果不是开发环境路由动态导入
+//todo 原本是可以用的，但是用上 eslint 之后，检测报错，草了。。。
 export default function _import(file, viewPath = 'views') {
-    if (process.env.NODE_ENV === 'development') {
+    // if (process.env.NODE_ENV === 'development') {
         return require(`@/${viewPath}/${file}.vue`).default// eslint-disable-line
-    }
-    if (process.env.NODE_ENV === 'production') {
-        return () => import(`@/${viewPath}/${file}.vue`)
-    }
-    return false
+    // }
+    // if (process.env.NODE_ENV === 'production') {
+    //     return () => import(`@/${viewPath}/${file}.vue`)
+    // }
+    // return false
+    // return () => import(`@/${viewPath}/${file}.vue`)
+
 }
